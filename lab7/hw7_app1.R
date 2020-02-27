5da2829e1fdc70a5258f7cae5892eac3f029e219
jasongreen271 <59592043+jasongreen271@users.noreply.github.com>
2020-02-27 10:01:57 -0800

    adding standalone r script

---
+++ lab7/hw7_app1.R
@@ -0,0 +1,31 @@
+if (!require("shinydashboard")) install.packages('shinydashboard')
+library("shinydashboard")
+
+UC_admit <- readr::read_csv("data/UC_admit.csv")
+
+UC_admit$Academic_Yr <- as.factor(UC_admit$Academic_Yr)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "UC Admissions by Ethnicity"),
+  dashboardSidebar(),
+  dashboardBody(
+    selectInput("fill", "Select variable", choices = c("Academic_Yr", "Campus", "Category"), 
+                selected = "Academic_Yr"),
+    plotOutput("plot", width = "900px", height = "600px"))
+)
+
+server <- function(input, output, session) { 
+  
+  output$plot <- renderPlot({
+    ggplot(UC_admit, aes_string(x = "Ethnicity", y="FilteredCountFR", fill=input$fill)) + 
+      geom_col(position = "dodge") + theme_light(base_size = 18) +
+      coord_flip()+
+      labs(y="Count")
+    
+  })
+  
+  session$onSessionEnded(stopApp)
+  
+}
+
+shinyApp(ui, server)
---
+++ lab7/hw7_app2.R
@@ -0,0 +1,30 @@
+if (!require("shinydashboard")) install.packages('shinydashboard')
+library("shinydashboard")
+
+UC_admit <- readr::read_csv("data/UC_admit.csv")
+
+UC_admit$Academic_Yr <- as.factor(UC_admit$Academic_Yr)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "UC Admissions by Academic Year"),
+  dashboardSidebar(),
+  dashboardBody(
+    selectInput("fill", "Select variable", choices = c("Campus", "Category", "Ethnicity"), 
+                selected = "Campus"),
+    plotOutput("plot", width = "900px", height = "600px"))
+)
+
+server <- function(input, output, session) { 
+  
+  output$plot <- renderPlot({
+    ggplot(UC_admit, aes_string(x = "Academic_Yr", y="FilteredCountFR", fill=input$fill)) + 
+      geom_col(position = "dodge") + theme_light(base_size = 18) +
+      coord_flip() +
+      labs(y="Count")
+  })
+  
+  session$onSessionEnded(stopApp)
+  
+}
+
+shinyApp(ui, server)
